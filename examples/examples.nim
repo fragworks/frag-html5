85c9d51258b021fb7463d73034ec5fd2183458b2
Zachary Carter <carterza@gmail.com>
2017-05-17 01:12:33 -0400

    Look & Feel updates for example page.

--- examples/examples.nim
+++ examples/examples.nim
@@ -8,9 +8,23 @@ proc example00(ev: Event, n: VNode) =
 
 proc createDom(): VNode =
   result = buildHtml(tdiv()):
-    a(class="button is-primary", onclick=example00):
-      text "Example 00"
-    tdiv(id="stage-container")
-    tdiv(id="stats")
+    tdiv(class="container"):
+      tdiv(class="columns"):
+        tdiv(class="column"):
+          aside(class="menu"):
+            p(class="menu-label"):
+              text "General"
+            ul(class="menu-list"):
+              li:
+                a(onclick=example00):
+                  text "Example 00"
+        tdiv(class="column"):
+          tdiv(class="card"):
+            tdiv(class="card-header"):
+              p(class="card-header-title"):
+                text "Example:"
+            tdiv(class="card-content", id="stage-container")
+            tdiv(class="card-footer"):
+              tdiv(class="card-footer-item", id="stats")
 
 setRenderer createDom
